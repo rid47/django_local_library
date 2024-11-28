71cb734f6af1be7c3baf746d818d935b32dcf7c1
Md. Ridwan Mizan <ridwanmizan@gmail.com>
2024-11-28 15:41:31 +0600

    shown visit count in home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -17,12 +17,19 @@ def index(request):
 
     num_genres = Genre.objects.count()
 
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
         
     }
 
